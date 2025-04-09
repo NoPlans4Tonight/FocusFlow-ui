ce4b084f7ca49101f1fefeffc1d3aac0598d13f9
NoPlans4Tonight <jlochan53@gmail.com>
2025-04-09 11:09:34 -0500

    refactor css for main application

--- src/App.vue
+++ src/App.vue
@@ -20,19 +20,26 @@ const isAuthenticated = !!useUserStore.user?.isAuthenticated
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+  width: 100%;
 }
 
 main {
   flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  margin: auto;
+  box-sizing: border-box;
 }
 
-footer {
+/* footer {
   text-align: center;
   margin-top: 2rem;
   font-size: 0.9rem;
   color: #888;
+} */
+
+@media (max-width: 768px) {
+  main {
+    padding: 1rem;
+  }
 }
 </style>
